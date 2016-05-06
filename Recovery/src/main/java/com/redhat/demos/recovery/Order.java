9ed720386f2d8d3b73b28cbff8c8c70dab444439
kavitha <>
2016-05-05 23:08:53 -0400

    {/Recovery/src/main/java/com/redhat/demos/recovery/Order.java}

--- Recovery/src/main/java/com/redhat/demos/recovery/Order.java
+++ Recovery/src/main/java/com/redhat/demos/recovery/Order.java
@@ -148,6 +148,7 @@ public class Order implements java.io.Serializable
    
    public String toString(){
        System.out.println("city"+city+"source: "+source);
+       return ("city"+city+"source: "+source);
    }
 
 
